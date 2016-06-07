36a6db8809db3f3f0d4745b70a8ad7a3982b34ab
DasOdom <DasOdom@users.noreply.github.com>
2016-06-07 09:52:15 -0400

    Add files via upload

---
+++ FRCMotorWithPot.ino
@@ -0,0 +1,21 @@
+#include <FRCmotor.h>
+
+FRCmotor DriveMotor;
+
+int gamemode = 1;
+int potpin = 0;
+int val;
+
+void setup()
+{
+  DriveMotor.SetPort(3);
+}
+
+void loop()
+{
+  val = analogRead(potpin);
+  val = map(val, 0, 1023, -100, 100);
+  DriveMotor.Set(val);
+  delay(15);
+
+}
